0a991c8550af708de83dcbe8ecab8df0f891a969
Волковская Ульяна Павловна <UVolkovskaya@inno.local>
2023-12-18 13:02:31 +0300

    feat: project innit

---
+++ src/main/java/com/isbd/utopiawebapp/UtopiaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.isbd.utopiawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtopiaWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isbd/utopiawebapp/UtopiaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isbd.utopiawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtopiaWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
