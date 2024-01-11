e639c9b82de4cda6ec8fc303a6a4e161151b77e4
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2024-01-11 16:21:19 +0300

    feat: page types

---
+++ frontend/src/api/types.ts
@@ -0,0 +1,25 @@
+export interface IPage {
+    pageable: Pageable;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size: number;
+    number: number;
+    sort: Sort;
+    numberOfElements: number;
+    empty: boolean;
+}
+export interface Pageable {
+    pageNumber: number;
+    pageSize: number;
+    sort: Sort;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+}
+export interface Sort {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+}
